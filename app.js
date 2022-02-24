adec61885b2486cf5250a83d5efdfaed33e73576
Polyuno <alamin.nava@gmail.com>
2022-02-24 17:22:25 +0600

    proper working

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 // form wrap
-const formFillUp = document.getElementById("formFillUp");
+const formSteps = Array.from(
+    document.querySelectorAll("#formFillUp .form-step")
+);
 // all form step
-const formOne = document.getElementById("formOne");
-const formTwo = document.getElementById("formTwo");
-const formThree = document.getElementById("formThree");
-// count step
-const stepPro = document.querySelector("#stepPro");
-const stepOne = document.querySelector(".stepOne");
-const stepTwo = document.querySelector(".stepTwo");
-const stepThree = document.querySelector(".stepThree");
+const formFillUp = document.getElementById("formFillUp");
+// // count step
+// const stepPro = document.querySelectorAll("#stepPro");
+const stepPro = Array.from(document.querySelectorAll(".stepPro .pro-step"));
+const hideStepPro = document.querySelector(".stepPro");
 // next btns
-const btnNextOne = document.querySelector(".btn-next-1");
-const btnNextTwo = document.querySelector(".btn-next-2");
+const btnNext = document.querySelectorAll(".btn-next");
 // prev btns
-const btnPrevOne = document.querySelector(".btn-prev-1");
-const btnPrevTwo = document.querySelector(".btn-prev-2");
+const btnPrev = document.querySelectorAll(".btn-prev");
 // submit btn
 const btnSubmit = document.querySelector(".btn-submit");
 // thank you page
 const tnxPage = document.querySelector(".thank-you");
 
-btnNextOne.addEventListener("click", function(e) {
-    e.preventDefault();
-    formTwo.style.left = `12px`;
-    formOne.style.left = `-380px`;
-    stepOne.classList.remove("step-active");
-    stepTwo.classList.add("step-active");
+btnNext.forEach((button) => {
+    button.addEventListener("click", () => {
+        changeStep("next");
+        prochangeStep("pro-next");
+    });
 });
-btnNextTwo.addEventListener("click", function(e) {
-    e.preventDefault();
-    formTwo.style.left = `-380px`;
-    formThree.style.left = `12px`;
-    stepTwo.classList.remove("step-active");
-    stepThree.classList.add("step-active");
+btnPrev.forEach((button) => {
+    button.addEventListener("click", () => {
+        changeStep("prev");
+        prochangeStep("pro-prev");
+    });
 });
 
-btnPrevOne.addEventListener("click", function(e) {
+formFillUp.addEventListener("submit", (e) => {
     e.preventDefault();
-    formOne.style.left = `12px`;
-    formTwo.style.left = `-380px`;
-    stepTwo.classList.remove("step-active");
-    stepOne.classList.add("step-active");
-});
-btnPrevTwo.addEventListener("click", function(e) {
-    e.preventDefault();
-    formThree.style.left = `-380px`;
-    formTwo.style.left = `12px`;
-    stepThree.classList.remove("step-active");
-    stepTwo.classList.add("step-active");
-});
+    const inputs = [];
 
-btnSubmit.addEventListener("click", function(e) {
-    e.preventDefault();
-    tnxPage.style.display = "block";
+    formFillUp.querySelectorAll("input").forEach((input) => {
+        const { name, value } = input;
+        inputs.push({ name, value });
+    });
+
     formFillUp.style.display = "none";
-    stepPro.style.display = "none";
+    hideStepPro.style.display = "none";
+    tnxPage.style.display = "block";
+
+    console.log(inputs);
+    formFillUp.reset();
 });
+
+let changeStep = function(btn) {
+    let index = 0;
+    let active = document.querySelector("#formFillUp .form-step.active");
+    index = formSteps.indexOf(active);
+
+    formSteps[index].classList.remove("active");
+    if (btn === "next") {
+        index++;
+    } else if (btn === "prev") {
+        index--;
+    }
+    formSteps[index].classList.add("active");
+};
+
+let prochangeStep = function(pro) {
+    let index = 0;
+    let active = document.querySelector(".stepPro .pro-step.step-active");
+
+    index = stepPro.indexOf(active);
+
+    stepPro[index].classList.remove("step-active");
+    if (pro === "pro-next") {
+        index++;
+    } else if (pro === "pro-prev") {
+        index--;
+    }
+    stepPro[index].classList.add("step-active");
+};
